c8592fe446f77b2990509911c23a0fc62cc6f29f
kenllyvera <josuevp2699@gmail.com>
2023-09-20 16:45:08 -0500

    arreglo de entidad

---
+++ tarea1/datos.js
@@ -0,0 +1,9 @@
+const triaje = [
+    {id: 1, fecha_cita: "20-09-2023", peso: "45 Kg", temperatura: "36 C", presion: "baja", id_usuario: 1},
+    {id: 2, fecha_cita: "21-10-2023", peso: "60 Kg", temperatura: "37 C", presion: "normal", id_usuario: 2},
+    {id: 3, fecha_cita: "22-11-2023", peso: "36 Kg", temperatura: "39 C", presion: "normal", id_usuario: 3},
+    {id: 4, fecha_cita: "23-12-2023", peso: "79 Kg", temperatura: "40 C", presion: "alta", id_usuario: 4},
+    {id: 5, fecha_cita: "24-12-2023", peso: "44 Kg", temperatura: "39 C", presion: "normal", id_usuario: 5},
+];
+
+module.exports = triaje;
